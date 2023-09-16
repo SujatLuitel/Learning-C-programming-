7400e3371469a055d14d61636b1c33e7a2971319
Sujat Luitel <107681024+SujatLuitel@users.noreply.github.com>
2023-09-16 10:58:37 +0545

    Update simple interest.c

--- C programs/simple interest.c
+++ C programs/simple interest.c
@@ -5,6 +5,6 @@ int main(){
 	printf("Enter principle, rate, and time:\n");
 	scanf("%d%f%d",&p,&r,&t);
 	si=(p*t*r)/100;
-	printf("The Simple Interest is %f",si);
+	printf("The Simple Interest is %.2f",si);
 	return 0;
 }
